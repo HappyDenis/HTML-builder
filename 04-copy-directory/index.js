dc166df40aacc5ba2d307e4a4146a82f1dfd299d
happydenis <dao.denis@gmail.com>
2023-05-09 18:11:37 +0300

    feat: add 4 solutions

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,28 @@
+const fsmodule = require("fs");
+const pathmodule = require("path");
 
+async function copyDir() {
+  await fsmodule.promises.mkdir(pathmodule.resolve(__dirname, "files-copy"), {
+    recursive: true,
+  });
+
+  let fileCopy = await fsmodule.promises.readdir(
+    pathmodule.resolve(__dirname, "files-copy")
+  );
+  for (const file of fileCopy) {
+    await fsmodule.promises.unlink(
+      pathmodule.resolve(__dirname, "files-copy", file)
+    );
+  }
+
+  let fileOrig = await fsmodule.promises.readdir(
+    pathmodule.resolve(__dirname, "files")
+  );
+  for (const file of fileOrig) {
+    await fsmodule.promises.copyFile(
+      pathmodule.resolve(__dirname, "files", file),
+      pathmodule.resolve(__dirname, "files-copy", file)
+    );
+  }
+}
+copyDir();
